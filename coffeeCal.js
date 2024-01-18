41ac6236b00861a2c27aaaca6fd01e6de84e3ea9
Daniel <simansky.dan@gmail.com>
2024-01-18 13:59:30 +0100

    Added water treatment option for users.

--- coffeeCal.js
+++ coffeeCal.js
@@ -3,25 +3,48 @@ const ratioInput = document.querySelector('.ratio');
 const waterInput = document.querySelector('.water');
 const yieldInput = document.querySelector('.coffee-yield');
 
-const calculateWater = (beans, ratio) => {
-    const water = beans * ratio;
-    const yield = water * 0.9;
+// Výpočet množstí beans (ratio T/F, ? pro podmínku, vrátí 0 když je T, když F tak provede výpoočet)
+const calculateBeans = (water, ratio) => ratio === 0 ? 0 : water / ratio;
+// Výpočet množství ratio
+const calculateRatio = (water, beans) => beans === 0 ? 0 : water / beans;
+// Výpočet množství water
+const calculateWater = (beans, ratio) => beans * ratio;
+// Výpočet množství yield (90%)
+const calculateYield = (water) => water * 0.9;
 
-    return { water, yield };
-};
+let currentInput = beansInput;
+let previousInput = beansInput;
 
 const userInput = (event) => {
-    const beans = beansInput.valueAsNumber || 0;
-    const ratio = ratioInput.valueAsNumber || 0;
+    // Načtení hodnot z polí pro beans, ratio a vody
+    let beans = beansInput.valueAsNumber || 0;       
+    let ratio = ratioInput.valueAsNumber || 0;
+    let water = waterInput.valueAsNumber || 0;
+    
+    if (currentInput !== event.target) {
+        previousInput = currentInput;
+        currentInput = event.target;
+    }  
 
-    const { water, yield } = calculateWater(beans, ratio);
-
-    if (event.target === beansInput) {
+    if ((currentInput === beansInput && previousInput === ratioInput) || (currentInput === ratioInput && previousInput === beansInput)) {
+        water = calculateWater(beans, ratio);
+        // Aktualizuje hodnoty vody
         waterInput.value = water;
+    } else if ((currentInput === waterInput && previousInput === ratioInput) || (currentInput === ratioInput && previousInput === waterInput)) { 
+        beans = calculateBeans(water, ratio);
+        // Aktualizuje hodnoty beans
+        beansInput.value = beans;
+    } else {
+        ratio = calculateRatio(water, beans);
+        // Aktualizuje hodnoty ratio
+        ratioInput.value = ratio;
     }
 
-    yieldInput.value = yield;
-};
+    // Aktualizace yield
+    const yieldResult = calculateYield(water);
+    yieldInput.value = yieldResult;
+}
 
 beansInput.addEventListener('input', userInput);
+waterInput.addEventListener('input', userInput);
 ratioInput.addEventListener('input', userInput);
